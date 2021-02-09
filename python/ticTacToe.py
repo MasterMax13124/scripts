b6c83a7eb668131a64c753df1f993762aff82d77
iamlambda <77165319+iamlambda@users.noreply.github.com>
2021-02-09 18:17:01 +0100

    adding a fake IA
    refactoring functions

--- python/ticTacToe.py
+++ python/ticTacToe.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
-def horizontalWin(board):
+from random import randint
+
+def horizontalWin(letter,board):
     for i in range(3):
-        if ( board[i][0] == "x" and board[i][1] == "x" and board[i][2] == "x") or ( board[i][0] == "o" and board[i][1] == "o" and board[i][2] == "o" ):
+        if ( board[i][0] == letter )  and ( board[i][1] == letter ) and ( board[i][2] == letter ):
             return True
 
-def verticalWin(board):
+def verticalWin(letter,board):
     for i in range(3):
-        if ( board[0][i] == "x" and board[1][i] == "x" and board[2][i] == "x") or ( board[0][i] == "o" and board[1][i] == "o" and board[2][i] == "o" ):
+        if ( board[0][i] == letter ) and ( board[1][i] == letter ) and ( board[2][i] == letter ):
             return True
 
-def diagonal(board):
+def diagonal(letter,board):
     #bottom left corner to up right corner
-    if ( board[2][0] == "x" and board[1][1] == "x" and board[0][2] == "x") or ( board[2][0] == "o" and board[1][1] == "o" and board[0][2] == "o" ):
+    if ( board[2][0] == letter ) and ( board[1][1] == letter ) and ( board[0][2] == letter ):
         return True
     #top left corner to down right corner
-    elif ( board[0][0] == "x" and board[1][1] == "x" and board[2][2] == "x") or ( board[0][0] == "o" and board[1][1] == "o" and board[2][2] == "o" ):
+    elif ( board[0][0] == letter ) and ( board[1][1] == letter ) and ( board[2][2] == letter ):
         return True
 
-def connected(board):
-    if horizontalWin(board) or verticalWin(board) or diagonal(board):
+def connected(letter, board):
+    if horizontalWin(letter, board) or verticalWin(letter, board) or diagonal(letter, board):
         return True
 
 def showBoard(board):
@@ -28,50 +30,105 @@ def showBoard(board):
         print("  "," -"*9)
     print(" "*5, "1", " "*3, "2", " "*3, "3","\n")
 
-print("\nWelcome to another random tic tac toe. \nPlease use the following syntax to play \"LetterNumber\", example: b2\nEnjoy! (:")
 
-board = [[" "," "," "],
-        [" "," "," "],
-        [" "," "," "]]
-moves = 0
+def twoPlayers(board):
+    moves = 0
+    while moves < 9:
+        showBoard(board)
+        if moves % 2 == 0:
+            player1 = input("Player 1's turn \n> ")
+            if player1 == "q" or player1 == "quit":
+                print("good bye (: ")
+                break
+            column = player1[0].lower()
+            row = int(player1[1])
+            case = board[-(ord(column)-95-1)][row-1] 
+            if case!=' ':
+                print("please play in an empty square.")
+                moves-=1
+            else:
+                board[-(ord(column)-96)][row-1] = "x"
+    
+        else:
+            player2 = input("Player 2's turn \n> ")
+            if player2 == "q" or player2 == "quit":
+                print("good bye (:")
+                break
+            column = player2[0].lower()
+            row = int(player2[1])
+            case = board[-(ord(column)-95-1)][row-1]
+            if case!= ' ':
+                print("please play in an empty square.")
+                moves-=1
+            else:
+                board[-(ord(column)-96)][row-1] = "o"
 
-while moves < 9:
-    showBoard(board)
-    if moves % 2 == 0:
-        print("Player 1's turn.")
-        player1 = input("> ")
-        if player1 == "quit":
-            print("good bye.")
+        #veryfing if someone won
+        if connected("x",board):
+            showBoard(board)
+            print("Player 1 won")
+            break 
+        elif connected("o", board):
+            showBoard(board)
+            print("Player 2 won")
+            break
+        moves += 1
+
+# fake IA, just ranodm moves 
+def singlePlayer(board):
+    moves = 0
+    while moves < 5:
+        showBoard(board)
+        #player turn 
+        player1 = input("Player 1's turn \n> ")
+        if player1 == "q" or player1 == "quit":
+            print("good bye (: ")
             break
         column = player1[0].lower()
         row = int(player1[1])
-        case = board[-(ord(column)-96)][row-1] 
+        case = board[-(ord(column)-95-1)][row-1] 
         if case!=' ':
             print("please play in an empty square.")
             moves-=1
         else:
             board[-(ord(column)-96)][row-1] = "x"
-            if connected(board):
-                showBoard(board)
-                print("Player 1 won.")
-                break
 
+        if connected("x",board):
+            showBoard(board)
+            print("You won ;)")
+            break 
+
+        #random oppenent moves
+        x,y = randint(0,2), randint(0,2)
+        while board[x][y] == "x" or board[x][y] == "o":
+            x,y = randint(0,2), randint(0,2)
+        board[x][y] = "o"
+
+        if connected("o",board):
+            showBoard(board)
+            print("The fake IA won :(")
+            break 
+
+        moves +=1 
+
+def main():
+    board = [[" "," "," "],
+            [" "," "," "],
+            [" "," "," "]]
+    
+    print("\nWelcome to another random tic tac toe. \nPlease use the following syntax to play \"LetterNumber\", example: b2\nEnjoy! (:")
+    
+    mod = input("What mod do you want to play? Single (s) or two players? (t) : ")
+    while mod != "s" and mod != "t":
+        mod = input("What mod do you want to play? Single (s) or two players? (t) : ")
+        if mod != 's' and mod != 't':
+            print('please insert either "s" or "t"')
+    if mod == "s":
+        singlePlayer(board)
     else:
-        print("Player 2's turn.")
-        player2 = input("> ")
-        if player2 == "quit":
-            print("good bye.")
-            break
-        column = player2[0].lower()
-        row = int(player2[1])
-        case = board[-(ord(column)-96)][row-1]
-        if case!= ' ':
-            print("please play in an empty square.")
-            moves-=1
-        else:
-            board[-(ord(column)-96)][row-1] = "o"
-            if connected(board):
-                showBoard(board)
-                print("Player 2 won.")
-                break
-    moves += 1
+        twoPlayers(board)
+    
+main()
+    
+    
+
